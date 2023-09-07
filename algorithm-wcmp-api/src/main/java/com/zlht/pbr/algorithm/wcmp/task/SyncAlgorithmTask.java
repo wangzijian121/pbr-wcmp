6cf23fd18882e3888de4043318a35ab1a72b6c08
wangzijian <1219116968@qq.com>
2023-09-07 14:37:44 +0800

    bug： Catch the RetryableException Exception  #6

--- algorithm-wcmp-api/src/main/java/com/zlht/pbr/algorithm/wcmp/task/SyncAlgorithmTask.java
+++ algorithm-wcmp-api/src/main/java/com/zlht/pbr/algorithm/wcmp/task/SyncAlgorithmTask.java
@@ -1,6 +1,9 @@
 package com.zlht.pbr.algorithm.wcmp.task;
 
 import com.zlht.pbr.algorithm.wcmp.service.AlgorithmConfigurationServiceI;
+import feign.RetryableException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -12,11 +15,18 @@ import org.springframework.stereotype.Component;
  **/
 @Component
 public class SyncAlgorithmTask {
+
+    private final static Logger logger = LogManager.getLogger(SyncAlgorithmTask.class);
     @Autowired
     private AlgorithmConfigurationServiceI algorithmConfigurationServiceI;
 
     @Scheduled(cron = "0/60 * *  * * ? ")
-    public void execute() {
-        algorithmConfigurationServiceI.syncAlgorithm();
+    public void executeSyncAlgorithm() {
+        try {
+            algorithmConfigurationServiceI.syncAlgorithm();
+        } catch (RetryableException retryableException) {
+            String errMsg = "同步算法失败！";
+            logger.warn("executeSyncAlgorithm(), message={}", errMsg);
+        }
     }
 }
